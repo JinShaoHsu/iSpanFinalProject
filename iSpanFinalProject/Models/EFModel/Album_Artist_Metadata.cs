f2d3469b4d2730cfec0a04382c3a17f5abdb173a
JinShaoHsu <sb850501@gmail.com>
2023-01-06 14:22:32 +0800

    Create AppDbContent
    Create AppDbContent

---
+++ iSpanFinalProject/Models/EFModel/Album_Artist_Metadata.cs
@@ -0,0 +1,21 @@
+namespace iSpanFinalProject.Models.EFModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Album_Artist_Metadata
+    {
+        public int id { get; set; }
+
+        public int albumId { get; set; }
+
+        public int artistId { get; set; }
+
+        public virtual Album Album { get; set; }
+
+        public virtual Artist Artist { get; set; }
+    }
+}
---
+++ iSpanFinalProject/Models/EFModel/Song_Creator_Metadata.cs
@@ -0,0 +1,21 @@
+namespace iSpanFinalProject.Models.EFModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Song_Creator_Metadata
+    {
+        public int id { get; set; }
+
+        public int songId { get; set; }
+
+        public int creatorId { get; set; }
+
+        public virtual Creator Creator { get; set; }
+
+        public virtual Song Song { get; set; }
+    }
+}
